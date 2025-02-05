192a6fcbe05143c2189f73219787e412b59f6b7a
honeypatel11 <honeypatel1109@gmail.com>
2025-02-05 17:13:55 +0530

    some deleted and added and amke clean

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-console.log("honey");
-
-document.write("jay bharat");
-
-window.alert("kuch bhii");
--- assets/js/variable.js
+++
@@ -1,9 +0,0 @@
-let a = 10;
-
-var b = "honey" + 10;
-
-const pi = 3.14;
-
-console.log(a);
-console.log(b);
-console.log(pi);
