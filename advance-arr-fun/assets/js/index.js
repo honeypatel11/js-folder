44d1da6945c6fd52fb992d828ef16dbb316aba49
honeypatel11 <honeypatel1109@gmail.com>
2025-02-19 16:16:50 +0530

    added for loop

---
+++ advance-arr-fun/assets/js/index.js
@@ -0,0 +1,33 @@
+let arr = [15, 24, 35, 40, 23];
+
+//normal for loop
+
+// for(let i=0; i<arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+
+//for of loop
+
+
+// let fruit = ["grapes" , "apple" , "cherry" , "banana" , "mango"];
+
+// for(let val of fruit){
+//     console.log(val);
+// }
+
+
+
+//for in loop
+
+let fruit = ["grapes", "apple", "cherry", "banana", "mango"];
+
+for (let val in fruit) {
+    console.log(val, fruit);
+}
+
+
+
+
+
+
---
+++ advance-arr-fun/assets/js/object.js
@@ -0,0 +1,22 @@
+let obj = {
+    name : "honey patel",
+    grid : 8174,
+    isDeveloper : true
+
+};
+
+
+//first way
+
+// console.log(obj);
+
+//second way
+// obj.name= "janvi";
+// console.log(obj.name);
+
+
+//third way(key value pair)
+
+// for(let key in obj){
+//     console.log(key);
+// }
