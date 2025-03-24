6fe22084e393adf6686d8420418e89d2753b1abe
honeypatel11 <honeypatel1109@gmail.com>
2025-03-24 16:58:23 +0530

    stu management added

---
+++ student management sys/index.js
@@ -0,0 +1,8 @@
+class Crud {
+    constructor (name,grid,email,phonenum){
+        this.name = name;
+        this.grid = grid;
+        this.email = email;
+        this.phonenum = phonenum;
+    }
+}
